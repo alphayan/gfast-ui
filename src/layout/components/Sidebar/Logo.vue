20d76d4395e0acbfeaa1ca0cc62d2331322ad525
alpha <alphayyq@163.com>
2020-12-22 15:32:42 +0800

    1

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -26,7 +26,7 @@ export default {
   },
   data() {
     return {
-      title: 'gfast管理系统',
+      title: 'cms管理系统',
       logo: logoImg
     }
   }
--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'gfast管理系统',
+  title: 'cms管理系统',
 
   /**
    * 是否系统布局配置
